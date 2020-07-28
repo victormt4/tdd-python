5805b96b44d8169c1ffc57fd91424a868608c392
Victor Mota <victor.vmt@outlook.com>
2020-07-28 17:03:53 -0300

    Removendo avaliador

--- src/leilao/dominio.py
+++ src/leilao/dominio.py
@@ -23,25 +23,18 @@ class Leilao:
     def __init__(self, descricao: str):
         self.descricao = descricao
         self.__lances = []
+        self.maior_lance = float_info.min
+        self.menor_lance = float_info.max
 
     def propor_lance(self, lance: Lance):
+
+        if lance.valor > self.maior_lance:
+            self.maior_lance = lance.valor
+        if lance.valor < self.menor_lance:
+            self.menor_lance = lance.valor
+
         self.__lances.append(lance)
 
     @property
     def lances(self):
         return self.__lances[:]
-
-
-class Avaliador:
-
-    def __init__(self):
-        self.maior_lance = float_info.min
-        self.menor_lance = float_info.max
-
-    def avalia(self, leilao: Leilao):
-
-        for lance in leilao.lances:
-            if lance.valor > self.maior_lance:
-                self.maior_lance = lance.valor
-            if lance.valor < self.menor_lance:
-                self.menor_lance = lance.valor
