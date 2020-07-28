70b5635a2203d497be840cc44c05f251f57a2a3d
Victor Mota <victor.vmt@outlook.com>
2020-07-28 17:04:29 -0300

    [Leilao] Refatorando testes

--- src/leilao/test_Leilao.py
+++ src/leilao/test_Leilao.py
@@ -1,9 +1,9 @@
 from unittest import TestCase
 
-from src.leilao.dominio import Usuario, Lance, Leilao, Avaliador
+from src.leilao.dominio import Usuario, Lance, Leilao
 
 
-class TestAvaliador(TestCase):
+class TestLeilao(TestCase):
 
     def setUp(self):
         self.gui = Usuario('Gui')
@@ -17,14 +17,11 @@ class TestAvaliador(TestCase):
         self.leilao.propor_lance(lance_yuri)
         self.leilao.propor_lance(self.lance_gui)
 
-        avaliador = Avaliador()
-        avaliador.avalia(self.leilao)
-
         menor_valor_esperado = 100.0
         maior_valor_esperado = 150.0
 
-        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
-        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
     def test_deve_retornar_valores_corretos_ao_inserir_em_ordem_decrescente(self):
         yuri = Usuario('Yuri')
@@ -33,23 +30,17 @@ class TestAvaliador(TestCase):
         self.leilao.propor_lance(self.lance_gui)
         self.leilao.propor_lance(lance_yuri)
 
-        avaliador = Avaliador()
-        avaliador.avalia(self.leilao)
-
         menor_valor_esperado = 100.0
         maior_valor_esperado = 150.0
 
-        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
-        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
     def test_deve_retornar_o_mesmo_valor_para_os_dois_lances_ao_inserir_um_lance(self):
         self.leilao.propor_lance(self.lance_gui)
 
-        avaliador = Avaliador()
-        avaliador.avalia(self.leilao)
-
-        self.assertEqual(150.0, avaliador.menor_lance)
-        self.assertEqual(150.0, avaliador.maior_lance)
+        self.assertEqual(150.0, self.leilao.menor_lance)
+        self.assertEqual(150.0, self.leilao.maior_lance)
 
     def test_deve_retornar_valores_corretos_ao_inserir_tres_lances(self):
         yuri = Usuario('Yuri')
@@ -61,8 +52,5 @@ class TestAvaliador(TestCase):
         self.leilao.propor_lance(lance_yuri)
         self.leilao.propor_lance(lance_vini)
 
-        avaliador = Avaliador()
-        avaliador.avalia(self.leilao)
-
-        self.assertEqual(100.0, avaliador.menor_lance)
-        self.assertEqual(200.0, avaliador.maior_lance)
+        self.assertEqual(100.0, self.leilao.menor_lance)
+        self.assertEqual(200.0, self.leilao.maior_lance)
