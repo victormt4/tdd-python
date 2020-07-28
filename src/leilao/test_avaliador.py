ead015a5baebcfd98cf046eb998418392e41636e
Victor Mota <victor.vmt@outlook.com>
2020-07-28 15:59:43 -0300

    [avaliador] adicionando mais 2 testes

--- src/leilao/test_avaliador.py
+++ src/leilao/test_avaliador.py
@@ -46,3 +46,36 @@ class TestAvaliador(TestCase):
 
         self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
         self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+
+    def test_deve_retornar_o_mesmo_valor_para_os_dois_lances_ao_inserir_um_lance(self):
+        gui = Usuario('Gui')
+        lance_gui = Lance(gui, 150.0)
+
+        leilao = Leilao('Celular')
+        leilao.lances.append(lance_gui)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        self.assertEqual(150.0, avaliador.menor_lance)
+        self.assertEqual(150.0, avaliador.maior_lance)
+
+    def test_deve_retornar_valores_corretos_ao_inserir_tres_lances(self):
+        gui = Usuario('Gui')
+        yuri = Usuario('Yuri')
+        vini = Usuario('Vini')
+
+        lance_gui = Lance(gui, 150.0)
+        lance_yuri = Lance(yuri, 100.0)
+        lance_vini = Lance(vini, 200.0)
+
+        leilao = Leilao('Celular')
+        leilao.lances.append(lance_gui)
+        leilao.lances.append(lance_yuri)
+        leilao.lances.append(lance_vini)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        self.assertEqual(100.0, avaliador.menor_lance)
+        self.assertEqual(200.0, avaliador.maior_lance)
